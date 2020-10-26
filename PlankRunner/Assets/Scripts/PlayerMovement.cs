bdb6312b7f408afda1e26c4466f290199fc6dd87
Elvis <letlive419@gmail.com>
2020-10-26 16:39:40 -0400

    troubleshooting movement

--- PlankRunner/Assets/Scripts/PlayerMovement.cs
+++ PlankRunner/Assets/Scripts/PlayerMovement.cs
@@ -7,25 +7,33 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] [Range(-3,3)] float movement;
     float left = -3;
     float right = 3;
+    float jump = 0;
+    float up = 3;
     Rigidbody rigid;
-    Vector3 pos;
+    Vector3 force;
+   
+    
     [SerializeField] float speed = 200;
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
-       
+        force = new Vector3(movement, jump * speed, transform.position.z);
         
 
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        
         transform.position = new Vector3(movement, transform.position.y, transform.position.z);
+        
         rigid.velocity = (Vector3.forward * Time.deltaTime * speed);
 
+        
+        
         if (Input.GetButtonDown("left") == true)
         {
             
@@ -47,7 +55,26 @@ public class PlayerMovement : MonoBehaviour
 
             }
         }
+        if (Input.GetButtonDown("up") == true)
+            if (transform.position.y > 0)
+                return;
+
+            else if (transform.position.y < 0)
+            {
+                jump = up;
+                rigid.AddForce(force);
+                print("jump");
+
+
+
+
+            }
+        
+    }
+    
+
+    
 
     }
     
-}
+
